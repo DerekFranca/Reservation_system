3abbc42aa019aedf55de20b40e5be6fef4abb30e
DaniloAire <Danilo.aires2015@gmail.com>
2025-01-22 20:58:05 -0400

    home

---
+++ home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="home.css">
+</head>
+<body>
+    <div class="principal">
+        <div class="meio">
+        <p class="titulo">Sistema de Reservas de espaços e areas comuns</p>
+        <a class="derek" href="">Derek interprise</a>
+        <Button class="botaohome">Cadastro de Espaços</Button>
+        <Button class="botaohome">Gerenciamento de Usuarios</Button>
+        <Button class="botaohome">Sistema de Reservas</Button>
+        </div>
+
+</div>
+</body>
+</html>
